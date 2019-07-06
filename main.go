ee86ef1078c915a1ad41ef13a1dea42ec8b6466b
Sergey Yanykin <syanykin@ozon.ru>
2019-07-06 20:27:33 +0300

    fix

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func main() {
 
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-	_, err = bot.SetWebhook(tgbotapi.NewWebhook(url + bot.Token))
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(url))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,7 +72,7 @@ func main() {
 	if info.LastErrorDate != 0 {
 		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
 	}
-	updates := bot.ListenForWebhook("/" + bot.Token)
+	updates := bot.ListenForWebhook("/" )
 	go http.ListenAndServe(":80", nil)
 	fmt.Println("Start serve")
 	for update := range updates {
